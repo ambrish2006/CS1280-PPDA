5349a04ff5d4460daa11e40bc4a40b14de084c4f
ambrish2006 <ambrishdhanveebbca24@rvu.edu.in>
2025-02-17 14:33:39 +0530

    Delete tutorial 2 directory

--- tutorial 2/Outerfunc.py
+++
@@ -1,7 +0,0 @@
-def outer_func():
-   global a
-   a=20
-   print("a =",a)
-a=10
-outer_func()
-print("global value of a=",a)
--- tutorial 2/parameters.py
+++
@@ -1,6 +0,0 @@
-def print_parameters(a, b, c):
-    print("1st param:", a)
-    print("2nd param:", b)
-    print("3rd param:", c)
-    
-print_parameters(a=46, b=69, c=99)
